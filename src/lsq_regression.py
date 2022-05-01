d1e62a0598f0707ebbbaa54a4ef9526a5d3e7c5c
fairwell-coding <coding@temmel.me>
2022-05-01 11:58:42 +0200

    up to task 5

--- src/lsq_regression.py
+++ src/lsq_regression.py
@@ -29,14 +29,14 @@ def task12():
         - each subplot must contain a title with the number of random features and the mean absolute difference between kernel and feature product.
     """
 
-    fig1, ax1 = plt.subplots(1, 3, figsize=(17,5))
+    fig1, ax1 = plt.subplots(1, 3, figsize=(17, 5))
     plt.suptitle('Task 1 - Regularized Least Squares and Double Descent Phenomenon', fontsize=16)
     for a in ax1.reshape(-1):
         a.set_ylim([0,40])
         a.set_ylabel('error')
         a.set_xlabel('number of random features')
 
-    fig2, ax2 = plt.subplots(1,3,figsize=(15,5))
+    fig2, ax2 = plt.subplots(1,3,figsize=(15, 5))
     plt.suptitle('Task 2 - Dual Representation with Kernel', fontsize=16)
 
     lams = [1e-8,1e-5,1e-3] # use this for subtask 1
@@ -46,7 +46,22 @@ def task12():
     """ Start of your code 
     """
 
-   
+    # parameters
+    d = 5
+    sigma = 2
+
+    # 4. Create training data
+    X, y = __create_data(200, d, sigma)
+
+    # 4. Create test data
+    X_t, y_t = __create_data(50, d, sigma)
+
+    # 5. Create random features
+    num_random_features = 50
+    V, _ = __create_data(num_random_features, d, sigma)
+
+    # 6. Implement w*
+    theta = 1 / np.sqrt(num_random_features) * (np.max())
 
     """ End of your code
     """
@@ -62,9 +77,17 @@ def task12():
     return fig1, fig2
 
 
+def __create_data(N, d, sigma):
+    data_points = np.random.rand(N, d)
+    X = data_points / np.linalg.norm(data_points, axis=1).reshape((N, 1))
+    y = np.reciprocal((1 / 4 + (X @ np.ones(d)) ** 2)) + np.random.normal(0, sigma ** 2)
+
+    return X, y
+
+
 if __name__ == '__main__':
     pdf = PdfPages('figures.pdf')
-    f1,f2 = task12()
+    f1, f2 = task12()
 
     pdf.savefig(f1)
     pdf.savefig(f2)
