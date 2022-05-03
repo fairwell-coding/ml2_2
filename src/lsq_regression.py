12f5f876c2405d016495a64cefc3b6e85f1d59b1
Ingmar <ingmar.lercher@hotmail.com>
2022-05-03 21:11:33 +0200

    different approach for theta matrix calculation

--- src/lsq_regression.py
+++ src/lsq_regression.py
@@ -32,7 +32,7 @@ def task12():
     fig1, ax1 = plt.subplots(1, 3, figsize=(17, 5))
     plt.suptitle('Task 1 - Regularized Least Squares and Double Descent Phenomenon', fontsize=16)
     for a in ax1.reshape(-1):
-        a.set_ylim([0,40])
+        #a.set_ylim([0,40])
         a.set_ylabel('error')
         a.set_xlabel('number of random features')
 
@@ -62,34 +62,46 @@ def task12():
     # V, _ = __create_data(num_random_features, d, sigma)
 
     feature_vectors = __create_feature_vectors(d)
-    train_losses = []
-    test_losses = []
+    train_losses = np.zeros((3, 61))
+    test_losses =  np.zeros((3, 61))
 
-    for V in feature_vectors:
-        I = np.diag(np.ones(V.shape[0]))
-        lambda_ = 1e-8  # lambda value to reproduce double desecent phenomenon
+    feature_index = 0
+    for r in range(5): # experiments
+        feature_vectors = __create_feature_vectors(d)
+        for l in range(3): # loop over lambdas
+            for v in range(0, 61): # loop over feature vectors
+                V = feature_vectors[v]
+                I = np.diag(np.ones(V.shape[0]))
+                lambda_ = lams[l]  # lambda value to reproduce double desecent phenomenon
 
-        # 1.6: Implement w* and calculate MSE
-        theta = __calculate_theta(V, X)
-        theta_t = __calculate_theta(V, X_t)
-        # R = lambda_ * I + theta.T @ theta
-        # z = theta.T @ y
+                # 1.6: Implement w* and calculate MSE
+                theta = __calculate_theta(V, X) #np.moveaxis(THETA, 1, 0) = THETA.T
+                theta_t = __calculate_theta(V, X_t)
+                # R = lambda_ * I + theta.T @ theta
+                # z = theta.T @ y
+                A = lambda_ * I + theta.T @ theta 
+                Q, R = np.linalg.qr(A)
+                z = Q.T @ theta.T @ y
+                # w_analytical = np.linalg.pinv(theta) @ y
 
-        A = lambda_ * I + theta.T @ theta
-        Q, R = np.linalg.qr(A)
-        z = Q.T @ theta.T @ y.reshape((N, 1))
-        w_analytical = np.linalg.inv(A) @ theta.T @ y
+                # 1.7: Reproduce double descent phenomenon
+                w_ml = np.linalg.solve(R, z)  # computation via QR decomposition
+                #w_ml = w_analytical
+                mse_train, mse_test = __perform_linear_regression(N, theta, theta_t, w_ml, y, y_t)
 
-        # 1.7: Reproduce double descent phenomenon
-        w_ml = np.linalg.solve(R, z)  # computation via QR decomposition
-        mse_train, mse_test = __perform_linear_regression(N, theta, theta_t, w_ml, y, y_t)
-        train_losses.append(mse_train)
-        test_losses.append(mse_test)
+                # maybe still some scaling problems
+                train_losses[l, v] += mse_train/d #average mse
+                test_losses[l, v] += mse_test/d
 
     """ End of your code
     """
+
+
     
+    features = range(61)
     for lam_idx, a in enumerate(ax1.reshape(-1)):
+        a.plot(features, train_losses[lam_idx, :], label='train')
+        a.plot(features, test_losses[lam_idx, :], label='test')
         a.legend()
         a.set_title(r'$\lambda=$'+str(lams[lam_idx]))
 
@@ -101,7 +113,12 @@ def task12():
 
 
 def __calculate_theta(V, X):
-    return 1 / np.sqrt(V.shape[0]) * (np.maximum(X @ V.T, 0))
+    theta = np.zeros((X.shape[0], V.shape[0], X.shape[1])) #  N x M x d -> example: 200 x 11 x 5 
+    for r in range(X.shape[1]):
+        v = V[:, r].reshape(V.shape[0], 1)
+        x = X[:, r].reshape(X.shape[0], 1)
+        theta[:, :, r] = 1 / np.sqrt(v.shape[0]) * (x @ v.T) 
+    return  (np.maximum(np.amax(theta, axis=2), 0)) # loog for max in each dimension d = 5
 
 
 def __perform_linear_regression(N, theta, theta_t, w_ml, y, y_t):
@@ -125,17 +142,20 @@ def __create_feature_vectors(d):
 
 
 def __create_data(N, d, sigma):
-    data_points = np.random.rand(N, d)
-    X = data_points / np.linalg.norm(data_points, axis=1).reshape((N, 1))
-    y = np.reciprocal((1 / 4 + (X @ np.ones(d)) ** 2)) + np.random.normal(0, sigma ** 2)
+    #data_points = np.random.uniform(size=(N, d))
+    X = np.random.uniform(size=(N, d))#  data_points / np.linalg.norm(data_points, axis=1).reshape((N, 1))
+    y = 1/((1 / 4 + (X @ np.ones(d)) ** 2)) + np.random.normal(0, sigma ** 2)
 
     return X, y
 
 
 if __name__ == '__main__':
-    pdf = PdfPages('figures.pdf')
-    f1, f2 = task12()
+    try:
+        pdf = PdfPages('figures.pdf')
+        f1, f2 = task12()
 
-    pdf.savefig(f1)
-    pdf.savefig(f2)
-    pdf.close()
+        pdf.savefig(f1)
+        pdf.savefig(f2)
+        pdf.close()
+    except:
+        pass
