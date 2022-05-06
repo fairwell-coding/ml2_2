afec9ca205ac4b6c2ab4faaae5fb657f9016c4a6
fairwell-coding <coding@temmel.me>
2022-05-06 11:37:24 +0200

    refactoring of theta and added two analytical solutions

--- src/lsq_regression.py
+++ src/lsq_regression.py
@@ -21,7 +21,7 @@ def task12():
         - labels for mean train/test errors are 'mean train error', 'mean test error' and must be included in the plots
 
         Subtask 2: Dual Representation with Kernel
-        
+
         Requirements for the plots:
         - make one subplot for each M
         - each subplot should contain the n=10th row of both the kernel matrix and the feature product \Phi\Phi^T
@@ -36,12 +36,12 @@ def task12():
         a.set_ylabel('error')
         a.set_xlabel('number of random features')
 
-    fig2, ax2 = plt.subplots(1,3,figsize=(15, 5))
+    fig2, ax2 = plt.subplots(1, 3, figsize=(15, 5))
     plt.suptitle('Task 2 - Dual Representation with Kernel', fontsize=16)
 
-    lams = [1e-8,1e-5,1e-3] # use this for subtask 1
-    m_array = [10,200,800] # use this for subtask 2
-    mae_array = 1e3*np.ones((3)) # use this for subtask 2 (MAE = mean absolute error)
+    lams = [1e-8, 1e-5, 1e-3]  # use this for subtask 1
+    m_array = [10, 200, 800]  # use this for subtask 2
+    mae_array = 1e3 * np.ones((3))  # use this for subtask 2 (MAE = mean absolute error)
 
     """ Start of your code 
     """
@@ -62,11 +62,11 @@ def task12():
     # V, _ = __create_data(num_random_features, d, sigma)
 
     train_losses = np.zeros((3, 61, 5))
-    test_losses =  np.zeros((3, 61, 5))
+    test_losses = np.zeros((3, 61, 5))
 
-    for l in range(3): # loop over lambdas
-        for v in range(0, 61): # loop over feature vectors
-            for r in range(5): # experiments
+    for l in range(3):  # loop over lambdas
+        for v in range(0, 61):  # loop over feature vectors
+            for r in range(5):  # experiments
                 V = __create_feature_vectors(v, d)
                 # V = feature_vectors[v]
                 I = np.diag(np.ones(V.shape[0]))
@@ -77,50 +77,48 @@ def task12():
                 theta_t = __calculate_theta(V, X_t)
                 # R = lambda_ * I + theta.T @ theta
                 # z = theta.T @ y
-                A = lambda_ * I + theta.T @ theta 
+                A = lambda_ * I + theta.T @ theta
                 Q, R = np.linalg.qr(A)
                 z = Q.T @ theta.T @ y
                 # w_analytical = np.linalg.pinv(theta) @ y
 
+                w_analytical_1 = np.linalg.inv(A) @ theta.T @ y
+                w_analytical_2 = np.linalg.pinv(A) @ theta.T @ y
+
                 # 1.7: Reproduce double descent phenomenon
                 w_ml = np.linalg.solve(R, z)  # computation via QR decomposition
-                #w_ml = w_analytical
+                # w_ml = w_analytical_2
                 mse_train, mse_test = __perform_linear_regression(N, theta, theta_t, w_ml, y, y_t)
 
                 # maybe still some scaling problems
-                train_losses[l, v, r] = mse_train #average mse
+                train_losses[l, v, r] = mse_train  # average mse
                 test_losses[l, v, r] = mse_test
+
+    # task 2: Dual representation
+
+
 
     """ End of your code
     """
 
-
-    
     features = range(61)
     train_mean = np.mean(train_losses, axis=2)
-    test_mean = np.mean(test_losses,  axis=2)
+    test_mean = np.mean(test_losses, axis=2)
     for lam_idx, a in enumerate(ax1.reshape(-1)):
         a.plot(features, train_mean[lam_idx, :], label='train')
         a.plot(features, test_mean[lam_idx, :], label='test')
         a.legend()
-        a.set_title(r'$\lambda=$'+str(lams[lam_idx]))
+        a.set_title(r'$\lambda=$' + str(lams[lam_idx]))
 
     for m_idx, a in enumerate(ax2.reshape(-1)):
         a.legend()
-        a.set_title('#Features M=%i, MAE=%f' %(m_array[m_idx],(mae_array[m_idx])))
+        a.set_title('#Features M=%i, MAE=%f' % (m_array[m_idx], (mae_array[m_idx])))
 
     return fig1, fig2
 
 
 def __calculate_theta(V, X):
-    theta = np.zeros((X.shape[0], V.shape[0], X.shape[1])) #  N x M x d -> example: 200 x 11 x 5 
-    for r in range(X.shape[1]):
-        v = V[:, r].reshape(V.shape[0], 1)
-        x = X[:, r].reshape(X.shape[0], 1)
-        theta[:, :, r] = 1 / np.sqrt(v.shape[0]) * (x @ v.T) 
-
-    return  (np.maximum(np.amax(theta, axis=2), 0)) # loog for max in each dimension d = 5
-    # return  (np.maximum(X @ V.T, 0)) # loog for max in each dimension d = 5
+    return 1 / np.sqrt(V.shape[0]) * (np.maximum(X @ V.T, 0))
 
 
 def __perform_linear_regression(N, theta, theta_t, w_ml, y, y_t):
@@ -134,17 +132,17 @@ def __perform_linear_regression(N, theta, theta_t, w_ml, y, y_t):
 
 def __create_feature_vectors(k, d):
     feature_vectors = []
-    N = 10*k + 1
+    N = 10 * k + 1
     data_points = np.random.rand(N, d)
-        # feature_vectors.append(data_points / np.linalg.norm(data_points, axis=1).reshape((N, 1)))
+    # feature_vectors.append(data_points / np.linalg.norm(data_points, axis=1).reshape((N, 1)))
 
     return data_points
 
 
 def __create_data(N, d, sigma):
-    #data_points = np.random.uniform(size=(N, d))
-    X = np.random.uniform(size=(N, d))#  data_points / np.linalg.norm(data_points, axis=1).reshape((N, 1))
-    y = 1/((1 / 4 + (X @ np.ones(d)) ** 2)) + np.random.normal(0, sigma ** 2)
+    # data_points = np.random.uniform(size=(N, d))
+    X = np.random.uniform(size=(N, d))  # data_points / np.linalg.norm(data_points, axis=1).reshape((N, 1))
+    y = 1 / ((1 / 4 + (X @ np.ones(d)) ** 2)) + np.random.normal(0, sigma ** 2)
 
     return X, y
 
